702411dd65f7c74c613e06d4c58e85e0a06c41db
yhzddy <1194725376@qq.com>
2019-10-31 18:17:26 +0800

    banner finish

---
+++ src/pages/Detail/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <DetailBanner></DetailBanner>
+    </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner.vue'
+
+export default {
+    name: 'Detail',
+    components:{
+        DetailBanner:DetailBanner
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,17 @@ import Router from 'vue-router'
 // import How from '../components/HelloWorld.vue'
 import Home from '../pages/Home/Home.vue'
 import City from '../pages/City/City.vue'
+import detail from '../pages/Detail/Detail.vue'
+
 
 
 Vue.use(Router)
 
 const routes =[
-    {path:'/',name:Home,component:Home},
-    {path:'/city',name:City,component:City},
+    {path:'/',name:'Home',component:Home},
+    {path:'/city',name:'City',component:City},
+    {path:'/detail/:id',name:'Detail',component:detail},
+
 ]
 
 
